cddcf8a14fa488117768a36a8dcdf639fd61b2b7
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-27 07:53:11 +0530

    Stepper Motor interface With Raspberry Pi PICO

---
+++ 18.Stepper motor/main.py
@@ -0,0 +1,21 @@
+################################
+#Stepper Motor interface With RPi Pico 
+# Author: Dharmendra Kumar Yadav
+#####################################
+from machine import Pin
+from time import sleep
+
+IN1 = Pin(2,Pin.OUT)
+IN2 = Pin(3,Pin.OUT)
+IN3 = Pin(4,Pin.OUT)
+IN4 = Pin(5,Pin.OUT)
+
+pins = [IN1, IN2, IN3, IN4]
+
+sequence = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+while True:
+    for step in sequence:
+        for i in range(len(pins)):
+            pins[i].value(step[i])
+            sleep(0.001)
