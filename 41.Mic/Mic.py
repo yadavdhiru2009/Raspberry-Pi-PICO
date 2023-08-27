5868405d253f0ad0d1064107cefdd821b3679600
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-08-27 08:15:18 +0530

    MIC interface With Raspberry Pi PICO

---
+++ 41.Mic/Mic.py
@@ -0,0 +1,26 @@
+################################
+# mic interface with RPi Pico 
+# Author: Dharmendra Kumar Yadav
+#####################################
+from machine import Pin,PWM
+from  time  import sleep_ms
+sound = Pin(0, Pin.IN, Pin.PULL_DOWN)  # Port internal pull-down
+Led_R = PWM(Pin(2))
+Led_G = PWM(Pin(3))
+Led_B = PWM(Pin(4))
+Led_R.freq(2000)   
+Led_G.freq(2000)  
+Led_B.freq(2000)   
+ 
+if __name__ == '__main__':
+    while True:
+        print(sound.value())
+        if sound.value() == 1:
+            Led_R.duty_u16(65535)
+            Led_G.duty_u16(65535)
+            Led_B.duty_u16(65535)
+            sleep_ms(2000)
+        else:
+            Led_R.duty_u16(0)
+            Led_G.duty_u16(0)
+            Led_B.duty_u16(0)
